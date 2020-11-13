bf4aa2c32e5186821bec0c833f720df190114ab4
Lucas <lcs.grc@outlook.com>
2020-11-13 16:16:56 +0100

    viewport_height.js - name modification + path

--- js/main.js
+++ js/main.js
@@ -1,7 +1,7 @@
 import { RubikCube } from "./RubikCube.js";
 import "./timer.js";
 import { resetTimer } from "./timer.js";
-import "./viewport_height.js";
+import "./style/mobile_viewport_height.js";
 
 const combination = document.querySelector("#combination p");
 const reloadBtn = document.querySelector(".fa-sync-alt");
